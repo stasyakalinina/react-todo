394b320acd3c134a6a41370725753130599044e8
Anastasia Kalinina <stasya.kalinina@gmail.com>
2020-02-05 18:25:17 +0500

    added listener and function for mark important

--- src/components/todo-item/todo-item.js
+++ src/components/todo-item/todo-item.js
@@ -4,40 +4,45 @@ import './todo-item.css';
 export default class TodoItem extends Component {
   state = {
     done: false,
+    important: false,
   };
 
   onLabelClick = () => {
     this.setState({
-      done: true
+      done: true,
+    });
+  }
+
+  onMarkImportant = () => {
+    this.setState({
+      important: true,
     });
   }
 
   render() {
-    const { label, important = false } = this.props;
-    const { done } = this.state;
+    const { label } = this.props;
+    const { done, important } = this.state;
 
     let classNames = 'todo-app__item';
     if (done) {
       classNames += ' is-done';
     }
-
-    const itemStyle = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+    if (important) {
+      classNames += ' is-important';
+    }
 
     return (
       <div className={classNames}>
         <span
           className="todo-app__label"
-          style={itemStyle}
           onClick={ this.onLabelClick }>
         {label}
         </span>
         <div className="todo-app__btns">
           <button
               type="button"
-              className="btn btn-outline-success btn-sm">
+              className="btn btn-outline-success btn-sm"
+              onClick={ this.onMarkImportant }>
               <i className="fa fa-exclamation"></i>
             </button>
             <button
