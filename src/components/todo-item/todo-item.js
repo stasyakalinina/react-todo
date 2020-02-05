5108a1e11949b4ab375861622ce8cdafb652405c
Anastasia Kalinina <stasya.kalinina@gmail.com>
2020-02-05 15:50:11 +0500

    added event listener on todo-item

--- src/components/todo-item/todo-item.js
+++ src/components/todo-item/todo-item.js
@@ -2,19 +2,36 @@ import React, { Component } from 'react';
 import './todo-item.css';
 
 export default class TodoItem extends Component {
+  state = {
+    done: false,
+  };
+
+  onLabelClick = () => {
+    this.setState({
+      done: true
+    });
+  }
 
   render() {
     const { label, important = false } = this.props;
+    const { done } = this.state;
+
+    let classNames = 'todo-app__item';
+    if (done) {
+      classNames += ' is-done';
+    }
+
     const itemStyle = {
       color: important ? 'steelblue' : 'black',
       fontWeight: important ? 'bold' : 'normal'
-    }
+    };
 
     return (
-      <div className="todo-app__item">
+      <div className={classNames}>
         <span
           className="todo-app__label"
-          style={itemStyle}>
+          style={itemStyle}
+          onClick={ this.onLabelClick }>
         {label}
         </span>
         <div className="todo-app__btns">
