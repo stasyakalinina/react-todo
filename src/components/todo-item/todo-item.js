83a92a3054ed4012ebe13db52adb3f72a7ca936e
Anastasia Kalinina <stasya.kalinina@gmail.com>
2020-02-05 12:57:23 +0500

    refactoring: smart components

--- src/components/todo-item/todo-item.js
+++ src/components/todo-item/todo-item.js
@@ -1,34 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './todo-item.css';
 
-const TodoItem = ({ label, important = false}) => {
-  const itemStyle = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
-  };
+export default class TodoItem extends Component {
 
-  return (
-    <div className="todo-app__item">
-      <span
-        className="todo-app__label"
-        style={itemStyle}>
-      {label}
-      </span>
-      <div className="todo-app__btns">
-        <button
-            type="button"
-            className="btn btn-outline-success btn-sm">
-            <i className="fa fa-exclamation"></i>
-          </button>
+  render() {
+    const { label, important = false } = this.props;
+    const itemStyle = {
+      color: important ? 'steelblue' : 'black',
+      fontWeight: important ? 'bold' : 'normal'
+    }
+
+    return (
+      <div className="todo-app__item">
+        <span
+          className="todo-app__label"
+          style={itemStyle}>
+        {label}
+        </span>
+        <div className="todo-app__btns">
           <button
-            type="button"
-            className="btn btn-outline-danger btn-sm">
-            <i className="fa fa fa-trash-o"></i>
-          </button>
+              type="button"
+              className="btn btn-outline-success btn-sm">
+              <i className="fa fa-exclamation"></i>
+            </button>
+            <button
+              type="button"
+              className="btn btn-outline-danger btn-sm">
+              <i className="fa fa fa-trash-o"></i>
+            </button>
+        </div>
       </div>
-    </div>
-
-  );
-};
-
-export default TodoItem;
+    );
+  }
+}
