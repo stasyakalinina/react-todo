d225dd4c661864d278d7dc9e16e07d895acfe24a
Anastasia Kalinina <stasya.kalinina@gmail.com>
2020-04-06 18:28:02 +0500

    Updates

---
+++ precache-manifest.ac4bf337295459d7d62dfdf6a4131324.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b81c7807d2789172bd9478375e02c7",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "b3a7dffe602ee66052e3",
+    "url": "/react-todo/static/css/main.a6534de9.chunk.css"
+  },
+  {
+    "revision": "532a3c6b7a2b83751e6b",
+    "url": "/react-todo/static/js/2.8e9e2458.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todo/static/js/2.8e9e2458.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b3a7dffe602ee66052e3",
+    "url": "/react-todo/static/js/main.1488d6db.chunk.js"
+  },
+  {
+    "revision": "3607658cb9c0653a6a9a",
+    "url": "/react-todo/static/js/runtime-main.63ea5226.js"
+  }
+]);
